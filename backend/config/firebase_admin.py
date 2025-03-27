e48ca5edbc4ac4db76cb091a550f03ff7c15f659
mertguney <yguney20@ku.edu.tr>
2025-03-27 18:04:49 +0300

    Change the path way for firebase credentials

--- backend/config/firebase_admin.py
+++ backend/config/firebase_admin.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
 firebase_admin.initialize_app(cred)
